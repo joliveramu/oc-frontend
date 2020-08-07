a7827e20a6f84368edafb0daab234c5f40895f4a
John Oliver Amurao <jayoliveramurao@gmail.com>
2020-08-08 01:21:16 +0800

    Modified step1.js and step2.js. Merging all objects to be used for the step1 and step2 files

--- P5_John Oliver/step1.js
+++ P5_John Oliver/step1.js
@@ -1,16 +1,85 @@
-// Initial values from the arrays below to form a quote
-let beginning = ["Hi! ","Yo! ","Hey "]
-let middle = ["What's up ","What up? ","How yo doin? "]
-let end = ["Dawg","Bro","Kid"]
 
-// Quote placeholder for quotes that will be generated from the beginning, middle, and end arrays
-let quote = "";
+//Step1.js
+let beginning = [
+            "I",
+            "You",
+            "He",
+            "She",
+            "It",
+            "We",
+            "They",
+            "One"
+];
 
-    var index_0 = Math.floor(Math.random() * beginning.length);
-    var index_1 = Math.floor(Math.random() * middle.length);
-    var index_2 = Math.floor(Math.random() * end.length);
-    quote = beginning[index_0] + middle[index_1] + end[index_2];
+let middle =[
+    "will be",
+    "should be",
+    "would be",
+    "shall be"
+];
 
-// Generated sentence
-console.log(quote);
+let ending =[
+    "the one to protect future",
+    "studying for our good",
+    "think ahead for the greater good"
+]
+
+let startCount = Math.floor(Math.random() * beginning.length);
+let middleCount = Math.floor(Math.random() * middle.length);
+let endCount = Math.floor(Math.random() * ending.length);
+
+
+function generateRandomQuote()
+{
+    return `${beginning[startCount]} ${middle[middleCount]} ${ending[endCount]}.`;
+}
+
+//Step 2 part
+
+let quotes = [
+    "",
+    "The man who does not read books has no advantage over the one who cannot read them.",
+    "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
+    "Teachers can open the door, but you must enter it yourself.",
+    "The beautiful thing about learning is that no one can take it away from you.",
+    "Education is the most powerful weapon you can use to change the world",
+    "The mind is not a vessel to be filled but a fire to be ignited.",
+    "Don’t let what you cannot do interfere with what you can do.",
+    "A person who never made a mistake never tried anything new."
+];
+
+let author = [
+    "",
+    "Mark Twain",
+    "Malcolm X",
+    "Chinese proverb",
+    "B.B. King",
+    "BB King",
+    "Plutarch",
+    "John Wooden",
+    "Albert Einstein"
+];
+
+function generateNQuotes(_number)
+{
+    let randomNum;
+    let isNumber = Number.isSafeInteger(_number);
+    if(isNumber && (_number >0 && _number  <= 5))
+    {
+        for(let i = 1; i <= _number; i++)
+        {
+            randomNum = Math.floor(Math.random() * quotes.length);
+            console.log(`"${quotes[randomNum]}" by ${author[randomNum]}\n`);
+        }
+    }
+}
+
+//Exported objects
+ module.exports = {
+    generateRandomQuote,
+    generateNQuotes
+ };
+
+
+console.log(generateRandomQuote());
 
--- P5_John Oliver/step2.js
+++ P5_John Oliver/step2.js
@@ -1,50 +1,70 @@
 const prompt = require('prompt-sync')();
+const tools = require('./step1.js');
 
-let quotes = [
-    "",
-    "The man who does not read books has no advantage over the one who cannot read them.",
-    "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
-    "Teachers can open the door, but you must enter it yourself.",
-    "The beautiful thing about learning is that no one can take it away from you.",
-    "Education is the most powerful weapon you can use to change the world",
-    "The mind is not a vessel to be filled but a fire to be ignited.",
-    "Don’t let what you cannot do interfere with what you can do.",
-    "A person who never made a mistake never tried anything new."
-];
+// let quotes = [
+//     "",
+//     "The man who does not read books has no advantage over the one who cannot read them.",
+//     "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
+//     "Teachers can open the door, but you must enter it yourself.",
+//     "The beautiful thing about learning is that no one can take it away from you.",
+//     "Education is the most powerful weapon you can use to change the world",
+//     "The mind is not a vessel to be filled but a fire to be ignited.",
+//     "Don’t let what you cannot do interfere with what you can do.",
+//     "A person who never made a mistake never tried anything new."
+// ];
 
-let author = [
-    "",
-    "Mark Twain",
-    "Malcolm X",
-    "Chinese proverb",
-    "B.B. King",
-    "BB King",
-    "Plutarch",
-    "John Wooden",
-    "Albert Einstein"
-];
+// let author = [
+//     "",
+//     "Mark Twain",
+//     "Malcolm X",
+//     "Chinese proverb",
+//     "B.B. King",
+//     "BB King",
+//     "Plutarch",
+//     "John Wooden",
+//     "Albert Einstein"
+// ];
 
 
 
-let enterValue;
-let decision;
-do
-{
-enterValue = prompt("Enter any number from 1 - 5: ");
+// let enterValue;
+// let decision;
+// do
+// {
+// enterValue = prompt("Enter any number from 1 - 5: ");
 
-if(enterValue >0 && enterValue  <= 5)
-{
-    for(let i = 1; i <= enterValue; i++)
+// if(enterValue >0 && enterValue  <= 5)
+// {
+//     for(let i = 1; i <= enterValue; i++)
+//     {
+//         let randomNum = Math.floor(Math.random() * quotes.length);
+//         console.log(`"${quotes[randomNum]}" by ${author[randomNum]}\n`);
+//     }
+// }
+
+// decision = prompt("Type any number to continue. 0 if would like to quit: ")
+
+
+// }while(decision != 0);
+let choice = 2;
+let isNumber;
+
+
+    switch(choice)
     {
-        let randomNum = Math.floor(Math.random() * quotes.length);
-        console.log(`"${quotes[randomNum]}" by ${author[randomNum]}\n`);
+        case 1: 
+            console.log(tools.generateRandomQuote());
+            break;
+        case 2:
+            tools.generateNQuotes(2);
+            break;
+        default:
+            break;
     }
-}
-
-decision = prompt("Type any number to continue. 0 if would like to quit: ")
 
 
-}while(decision != 0);
+
+
 
 
 
