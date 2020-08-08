ced903f1c7fae4807b1cb419bec30b894b650579
John Oliver Amurao <jayoliveramurao@gmail.com>
2020-08-09 05:36:58 +0800

    Added P6 folder with initial boilerplate.

---
+++ P6_John Oliver/assets/js/index.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("gameCanvas");
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'red';
+context.fillRect(20,20,100,100);
+
+
+context.fillStyle = 'blue';
+context.fillRect(600,50, 100,100);
