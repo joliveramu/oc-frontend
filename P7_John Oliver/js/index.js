5ab27de7e2c2d15b15070f3569c797dd5a970516
John Oliver Amurao <jayoliveramurao@gmail.com>
2020-11-22 04:00:35 +0800

    Added project 7, leaflet js, and generated static map.

--- P7_John Oliver/js/index.js
+++ P7_John Oliver/js/index.js
@@ -8,22 +8,65 @@ class Main extends React.Component {
   render() {
     return(
     <div>
-      <Oliver 
-      name="John Oliver," 
-      lastName="Amurao" />
+      <Header />
+      <Map />
+      <LeaveReview />
+      <Footer />
     </div>);
   }
 }
 
-function Oliver({name, lastName})
+let Header = () =>
 {
   return(
-    <div>
-      My name is 
-      <h1>{name} {lastName}</h1>
+    <div className="fixed-top container-fluid navbar-expand-md navbar-light bg-info">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li class="nav-item">
+            <a className="navbar-brand" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="navbar-brand" href="#">Restaurant Maps</a>
+          </li>
+          <li className="nav-item">
+            <a className="navbar-brand" href="#">Leave a Review</a>
+          </li>
+
+        </ul>
+      </div>
     </div>
   );
 }
 
+let Map = () =>
+{
+  return(
+    <div className = "mt-5">
+      <h1>This is Map!</h1>
+      
+    </div>
+  );
+}
+
+let LeaveReview = () =>
+{
+  return(
+    <div className = "m5">
+      <h1>Leave Review!</h1>
+    </div>
+  )
+}
+
+let Footer = () =>
+{
+  return(
+    <div className = "fixed-bottom container-fluid">
+      <h1>This footer</h1>
+    </div>
+  );
+}
 
 ReactDOM.render(<Main />, document.getElementById('example'))
