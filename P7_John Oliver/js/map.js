9ff329e98d9ec143cac88d017178a44a57ea4992
John Oliver Amurao <jayoliveramurao@gmail.com>
2020-12-26 01:27:06 +0800

    Added functionality for street view

--- P7_John Oliver/js/map.js
+++ P7_John Oliver/js/map.js
@@ -4,12 +4,17 @@ var map = L.map('map').setView([14.5552734,120.9999306], 13);
 L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=rbSj7RYeRSmrshxqhdgx',{
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
+
 var marker = L.marker([14.5552734,120.9999306]).addTo(map);
+
 
 //Display all restaurants in the leaflet map using their lat lng coordinates
 for(let i = 0; i < data.length; i++)
 {
+
     L.marker([data[i].lat, data[i].long]).addTo(map).bindPopup(data[i].restaurantName).openPopup();
+    L.circle([data[i].lat, data[i].long]).addTo(map);
+
 }
 
 
@@ -21,9 +26,13 @@ map.locate({setView: true, maxZoom: 16});
 function onLocationFound(e) {
     var radius = e.accuracy;
 
+    //Display image of the place when being viewed
+    L.streetView({ position: 'topleft', mapillaryId: 'OTNpQldXOGpGWkNmUHc5SU9wTk9zSDphMjBmMGE3ZmUzMTc4MjUw' }).addTo(map);
+    
     L.marker(e.latlng).addTo(map)
         .bindPopup(`You are currently here @ ${e.latlng}`).openPopup();
 
+    //L.streetView({ position: 'topleft', mapillaryId: 'RC1ZRTBfaVlhWmJmUGVqRk5CYnAxQTpmMGE3OTU0MzM0MTljZTA4' }).addTo(map);
     L.circle(e.latlng).addTo(map);
 }
 
@@ -53,6 +62,8 @@ providers: [
 ]
 }).addTo(map);
     
+
+
 
     // function onMapClick(e) {
     //     alert("You clicked the map at " + e.latlng);
