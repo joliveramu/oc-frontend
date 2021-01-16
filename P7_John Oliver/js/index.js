bce5528c24afa3c501d8d380c0c7a3fc0239ae61
John Oliver Amurao <jayoliveramurao@gmail.com>
2021-01-16 15:26:06 +0800

    P7 code clean-up. Fixed filtering query.

--- P7_John Oliver/js/index.js
+++ P7_John Oliver/js/index.js
@@ -29,13 +29,16 @@ fetch("js/data.json")
       msg += "</a><br/>";
       document.querySelector('#list').innerHTML +=  msg; 
       sum = 0;
+
+       //Display markers of each location
+        L.marker([record.lat, record.long]).addTo(map).bindPopup(`${record.restaurantName} <label class ='text-warning'>${parseFloat(avg).toFixed(2)} stars</label> <br> <img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[record.lat,record.long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`).openPopup();
+        L.circle([record.lat, record.long]).addTo(map);
   }); 
 
   });
   
   function loadDataPersistent()
   {
-    
     query += alasql("CREATE TABLE restaurant(id integer not null primary key autoincrement, resto_name text, lat real NOT NULL ,long real NOT NULL);");
     query += alasql("insert into restaurant (resto_name, lat, long) values ('AU 2552', 14.5434407, 121.0045482);"); 
     query += alasql("insert into restaurant (resto_name, lat, long) values ('CKA Jollyplate Eatery', 14.5431196, 121.0026515);");
@@ -60,9 +63,8 @@ fetch("js/data.json")
     query2 += alasql("insert into restaurant_review (restaurant_id, stars, comment) values (6, 4,'food is quite acceptable');")
     let display2 = alasql("select * from restaurant_review;");
     console.log(display2);
-    
-
   }
+
   function loadRestaurants()
   {
     document.querySelector('#restaurants').innerHTML = null;
@@ -95,7 +97,7 @@ function searchRecord()
   let iterator = 1;
     if(range2.value < range1.value)
     {
-      alert("Second Parameter must be greater than the 1st parameter");
+      // alert("Second Parameter must be greater than the 1st parameter");
     }else{
       $(".leaflet-marker-icon").remove();
         $(".leaflet-popup").remove();
@@ -128,7 +130,7 @@ function searchRecord()
 
             msg += `<img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data.lat,data.long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`;
             msg += `</ul>`;
-            msg +=  `<small class='text-muted'><b class = 'average'></b></small>`;
+            msg +=  `<small class='text-muted'><b class = 'average'>${data.Ratings.toFixed(2)}</b></small>`;
             msg += "</a><br/>";
             document.querySelector('#list').innerHTML +=  msg; 
             
@@ -136,151 +138,15 @@ function searchRecord()
             L.marker([data.lat, data.long]).addTo(map).bindPopup(`${data.resto_name} <label class ='text-warning'>${parseFloat(data.Ratings).toFixed(2)} stars</label> <br> <img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data.lat,data.long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`).openPopup();
             L.circle([data.lat, data.long]).addTo(map);
           }
-         
-          console.log(revs);
         });
-
-      //   reviewsQuery.forEach(function(record){
-      //     console.log(`${record.restaurant_id} ${record.stars}`);
-      // });
-
-
-        // for(let i = 0; i < data.length; i++)
-        //     {
-    
-        //       for(let j = 0; j < data[i].ratings.length; j++)
-        //       {
-        //           sum = sum + data[i].ratings[j].stars;
-        //           avg = sum / data[i].ratings.length;
-        //       }
-        //       sum = 0;
-      
-    
-                
-        //         if(avg >= range1.value && avg <= range2.value)
-        //         {
-    
-        //           //Display lists of restaurants
-        //           msg = "<a class='list-group-item list-group-item-action flex-column align-items-start'>";
-        //           msg += "<div class='d-flex w-100 justify-content-between'>";
-        //           msg += `<h5 class='mb-1 text-info'>${data[i].restaurantName}</h5>`;
-        //           msg += `<small class='text-warning font-weight-bold'></small>`;
-        //           msg += "</div>"; 
-        //           msg += `<p class='mb-1 text-truncate'></p>`;
-        //           msg += `<ul>`;
-    
-        //           for(let j = 0; j < data[i].ratings.length; j++)
-        //           {
-                   
-        //             msg +=  `<li><small class='text-muted'><b>${data[i].ratings[j].comment}</b></small></li>`;
-                   
-        //           }
-    
-        //           msg += `<img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data.lat,data.long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`;
-        //           msg += `</ul>`;
-        //           msg +=  `<small class='text-muted'><b class = 'average'>${avg}</b></small>`;
-        //           msg += "</a><br/>";
-        //           document.querySelector('#list').innerHTML +=  msg; 
-                  
-        //           //Display markers of each location
-        //           L.marker([data[i].lat, data[i].long]).addTo(map).bindPopup(`${data[i].restaurantName} <label class ='text-warning'>${parseFloat(avg).toFixed(2)} stars</label> <br> <img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data[i].lat,data[i].long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`).openPopup();
-        //           L.circle([data[i].lat, data[i].long]).addTo(map);
-
-      // end of search query
-      
-      //   document.querySelector('#restaurants').innerHTML = null;
-      //   msg += `<select> <option value =''>Select Restaurant</option>`;
-      // let query = alasql(`SELECT r.resto_name, avg(rr.stars) AS Ratings from restaurant as r INNER JOIN restaurant_review as rr ON r.id = rr.restaurant_id where rr.stars between ${range1.value} and ${range2.value} group by r.resto_name;`);
-      // query.forEach(function(data){
-      //   msg += `<option value ='${data.resto_name}'>${data.resto_name}</option>`;
-        
-      // });
-      //   msg += `</select>`;
-      //   document.querySelector('#restaurants').innerHTML +=  msg;
-
-      // fetch("js/data.json")
-      // .then((res) => res.json())
-      // .then((data) => {
-      //   // map.eachLayer((layer) => {
-      //   //   layer.remove();
-      //   // });
-      //   $(".leaflet-marker-icon").remove();
-      //   $(".leaflet-popup").remove();
-      //   $(".leaflet-marker-shadow").remove();
-      //   document.querySelector('#list').innerHTML = null;
-      //   // document.querySelector('#map').innerHTML = null;
-      //     for(let i = 0; i < data.length; i++)
-      //     {
-  
-      //       for(let j = 0; j < data[i].ratings.length; j++)
-      //       {
-      //           sum = sum + data[i].ratings[j].stars;
-      //           avg = sum / data[i].ratings.length;
-      //       }
-      //       sum = 0;
-    
-  
-              
-      //         if(avg >= range1.value && avg <= range2.value)
-      //         {
-  
-      //           //Display lists of restaurants
-      //           msg = "<a class='list-group-item list-group-item-action flex-column align-items-start'>";
-      //           msg += "<div class='d-flex w-100 justify-content-between'>";
-      //           msg += `<h5 class='mb-1 text-info'>${data[i].restaurantName}</h5>`;
-      //           msg += `<small class='text-warning font-weight-bold'></small>`;
-      //           msg += "</div>"; 
-      //           msg += `<p class='mb-1 text-truncate'></p>`;
-      //           msg += `<ul>`;
-  
-      //           for(let j = 0; j < data[i].ratings.length; j++)
-      //           {
-                 
-      //             msg +=  `<li><small class='text-muted'><b>${data[i].ratings[j].comment}</b></small></li>`;
-                 
-      //           }
-  
-      //           msg += `<img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data.lat,data.long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`;
-      //           msg += `</ul>`;
-      //           msg +=  `<small class='text-muted'><b class = 'average'>${avg}</b></small>`;
-      //           msg += "</a><br/>";
-      //           document.querySelector('#list').innerHTML +=  msg; 
-                
-      //           //Display markers of each location
-      //           L.marker([data[i].lat, data[i].long]).addTo(map).bindPopup(`${data[i].restaurantName} <label class ='text-warning'>${parseFloat(avg).toFixed(2)} stars</label> <br> <img src='https://maps.googleapis.com/maps/api/streetview?size=200x200&location=${[data[i].lat,data[i].long]}&fov=80&heading=70&pitch=0&key=AIzaSyCjeBzfVtmlDu9mRUIpJbCK_ekqSimIVB4'/>`).openPopup();
-      //           L.circle([data[i].lat, data[i].long]).addTo(map);
-               
-      //           console.log(`${data[i].restaurantName} ${avg}`);
-               
-                
-      //         }
-      //     }
-       
-      // });
-
-
     }
-    
-  
 }
 
 
-//Bootstrap filter function using JQuery
-  // $(document).ready(function(){
-  //   $("#rating").change(function() {
-  //     var value = $(this).val().toLowerCase();
-  //     // $("#list a").filter(function() {
-  //     //   $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-  //     // });
-  //     $("#list a").show().filter(function() {
-  //       return $(this).text().toLowerCase().trim().indexOf(value) == -1;
-  //     }).hide();
-  //   });
-  // });
 frmReview.addEventListener('submit',(event) =>{
   event.preventDefault();
 
-  let isOkay = alasql(`insert into restaurant_review (restaurant_id, stars, comment) values (${cmbRestaurant.value}, ${txtStars.value}, '${txtReview.value}');`);
+  let isOkay = alasql(`insert into restaurant_review (restaurant_id, stars, comment) values (${cmbRestaurant.value}, ${txtStars.value}, '${txtReview.value.replaceAll("'","\\'")}');`,);
   if(isOkay === 1)
   {
     alert("Review for the restaurant is added!!");
